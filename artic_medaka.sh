119fb5bccf4bd7ac424ff77fe07620425e16da99
George Githinji <georgkam@gmail.com>
2022-08-16 12:41:15 +0300

    changed command line argument flags

--- artic_medaka.sh
+++ artic_medaka.sh
@@ -8,19 +8,18 @@ set -e
 # https://artic.network/ncov-2019/ncov2019-bioinformatics-sop.html
 # Version 0.0.1
 # Written by George Githinji
-# License GNU 
+# License GNU
 #########################################
+
+USAGE="USAGE $0 -f sample_manifest_file.csv -r run_name -i input_fastq_files -p path/to/primer/scheme -n normalisation -t threads -k bam"
 
 echo "Activating the artic workflow...."
 
 # Activate conda enviroment, assumes that miniconda is installed in the home directory
 source "${HOME}/miniconda3/etc/profile.d/conda.sh"
 
-# Activate the artic conda enviroment, 
+# Activate the artic conda enviroment,
 conda activate artic
-
-USAGE="USAGE $0 -f sample_manifest_file.csv -r run_name -q input_fastq_files -s path/to/primer/scheme -n normalisation -t threads -k bam"
-
 
 SCHEME_DIR="${HOME}/artic-ncov2019/primer_schemes" # Artic scheme directory
 MEDAKA_MODEL="r941_min_high_g360"                  # medaka model
@@ -35,24 +34,24 @@ intermediate_files_less_bam=("*.vcf"  "*.gz" "*.tbi" "*.txt" "*.er" "*.depths" "
 # A function that deletes all intermediate files
 delete_all(){
     for ((i=0; i<${#intermediate_files[@]}; i++)); do
-        find . -name "${intermediate_files[$i]}" -type f -delete || continue
+        find . -maxdepth 1 -name "${intermediate_files[$i]}" -type f -delete || continue
     done
 }
 
 # Delete all finds except the bam files
 keep_bam(){
     for ((i=0; i<${#intermediate_files_less_bam[@]}; i++)); do
-        find . -iname "${intermediate_files_less_bam[$i]}" -type f -delete || continue
+        find . -maxdepth 1 -name "${intermediate_files_less_bam[$i]}" -type f -delete || continue
     done
 }
 
-while getopts f:r:q:s:n:t:k:h opt
+while getopts f:r:i:p:n:t:k:h opt
 do
     case "$opt" in
         f) SAMPLE_NAMES="$OPTARG";;
         r) RUN_NAME="$OPTARG";;
-        q) FASTQ_FILES="$OPTARG";;
-        s) SCHEME_DIR="$OPTARG";;
+        i) FASTQ_FILES="$OPTARG";;
+        p) SCHEME_DIR="$OPTARG";;
         n) NORMALISE_VALUE="$OPTARG";;
         t) THREADS="$OPTARG";;
         k) KEEP="$OPTARG";;
@@ -61,7 +60,7 @@ do
     esac
 done
 
-# enforce "run name", "sample manifest file path", "fastq directory path, and number of threads command option are provided by the user 
+# enforce "run name", "sample manifest file path", "fastq directory path, and number of threads command option are provided by the user
 if [[ $RUN_NAME == "" || $SAMPLE_NAMES == "" || $FASTQ_FILES == "" || $THREADS == "" ]]; then
     echo $USAGE
     exit 0
@@ -71,11 +70,11 @@ fi
 # TODO: optimize this loop to run in parallel
 while IFS=, read -r barcode sample_name
 do
-	if [ -z "${barcode}" ] || [ -z "${sample_name}" ]; then 
+	if [ -z "${barcode}" ] || [ -z "${sample_name}" ]; then
 
         echo "Missing barcode or matching sample_id, skipping this entry"
 
-    else 
+    else
 
         echo "Processing "$sample_name" "
 
@@ -97,14 +96,14 @@ do
             nCoV-2019/V4.1 "${sample_name}_${RUN_NAME}" || continue
 
         if [ "$KEEP" == "none" ]; then
-            echo "Removing intermediate files"   
+            echo "Removing intermediate files"
             delete_all
         elif [ "$KEEP" == "bam" ]; then
             echo "Retained the BAM and corresponding bam in file"
             keep_bam
         elif [ "$KEEP" == "all" ]; then
             echo "Retaining intermediate files..."
-        fi   
+        fi
     fi
 
 done <$SAMPLE_NAMES
